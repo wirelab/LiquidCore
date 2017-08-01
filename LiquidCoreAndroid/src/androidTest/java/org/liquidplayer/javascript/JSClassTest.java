2a6cb3535c7f5fbb4d485f259963695c50b86945
Coen Wessels <c.dewessels@gmail.com>
2017-08-01 16:27:49 +0200

    Added tests for JSClass

---
+++ LiquidCoreAndroid/src/androidTest/java/org/liquidplayer/javascript/JSClassTest.java
@@ -0,0 +1,51 @@
+package org.liquidplayer.javascript;
+
+import junit.framework.Assert;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by cwessels on 01/08/2017.
+ */
+
+public class JSClassTest {
+    public class TestObj extends JSInstance {
+        public final String _param;
+
+        public boolean called = false;
+
+        public TestObj(JSObject value, String param) {
+            super(value);
+            _param = param;
+        }
+
+        public void doThings() {
+            called = true;
+        }
+    }
+
+    public class TestObjClass extends JSClass<TestObj> {
+        public TestObjClass(JSContext jsContext) {
+            super(jsContext, TestObjClass.class, TestObj.class);
+        }
+
+        public TestObj create() {
+            return new TestObj(newInstance(), "Test!");
+        }
+    }
+
+    @org.junit.Test
+    public void testJSClass() {
+        JSContext jsContext = new JSContext();
+        TestObjClass klass = new TestObjClass(jsContext);
+        jsContext.property("TestObj", klass);
+
+        jsContext.evaluateScript("var testObj = TestObj.create();");
+        TestObj testObj = (TestObj)jsContext.evaluateScript("testObj").toObject();
+
+        assertFalse(testObj.called);
+        jsContext.evaluateScript("testObj.doThings()");
+        assertTrue(testObj.called);
+    }
+}
